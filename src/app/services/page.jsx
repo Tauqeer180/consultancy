0a0f5ffc3090f46dfab158b52f9c751db6a4263d
tauqeer180 <tauqeermalik855@gmail.com>
2024-07-10 15:40:12 +0500

    servces

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -8,7 +8,7 @@ export default function page() {
   return (
     <div className=" max-w-5xl lg:px-11 sm:px-4 mx-auto pb-20">
       <h2 className="sm:pb-11 pb-2 sm:pt-20 pt-16 text-center md:text-6xl sm:text-4xl text-3xl uppercase tracking-wider">
-        About
+        Services
       </h2>
       <div className=" md:space-y-20 ">
         {Services?.map(({ image, title, price, time }, i) => {
