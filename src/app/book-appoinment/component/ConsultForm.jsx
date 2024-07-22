ba51aaf0321923fe8db34b7481e4c71c541ae4fc
tauqeer180 <tauqeermalik855@gmail.com>
2024-07-22 20:05:33 +0500

    booking Form

--- src/app/book-appoinment/component/ConsultForm.jsx
+++ src/app/book-appoinment/component/ConsultForm.jsx
@@ -20,36 +20,68 @@ let service = [
   "career consulting",
   "medical consulting",
 ];
-let de_visa_type = [
-  "study visa",
-  "au pair visa",
-  "vocational training visa",
-  "family reunion visa (FRV)",
-  "job seeking visa",
-  "visit visa",
-  "qualification recognition",
-  "document preperations",
-  "interview preperations",
-  "attestations and verifications",
-  "other",
-];
-let uk_visa_type = [
-  "study visa",
-  "visit visa",
-  "FRV",
-  "document preparations",
-  "interview preparations",
-  "attestations and verifications",
-];
-let india_visa_type = [
-  "visit visa (for non indians)",
-  "tourist visa (non indians)",
-  "document verifications (indians)",
-  "document attestations (indians)",
-  "notary (indians)",
-  "other",
-];
+let visa_types = {
+  germany: [
+    "study visa",
+    "au pair visa",
+    "vocational training visa",
+    "family reunion visa (FRV)",
+    "job seeking visa",
+    "visit visa",
+    "qualification recognition",
+    "document preperations",
+    "interview preperations",
+    "attestations and verifications",
+    "other",
+  ],
+  uk: [
+    "study visa",
+    "visit visa",
+    "FRV",
+    "document preparations",
+    "interview preparations",
+    "attestations and verifications",
+  ],
+  india: [
+    "visit visa (for non indians)",
+    "tourist visa (non indians)",
+    "document verifications (indians)",
+    "document attestations (indians)",
+    "notary (indians)",
+    "other",
+  ],
+};
 
+let edu_types = {
+  germany: [
+    "german universities selection",
+    "german universities course selection",
+    "masters",
+    "bachelors",
+    "phd",
+    "ielts bands",
+    "german language level",
+  ],
+  uk: [
+    "uk universities selection",
+    "uk universities course selection",
+    "masters",
+    "bachelors",
+    "other (please specify)",
+  ],
+  india: [
+    "nursing college admissions",
+    "nursing college selection",
+    "nursing course planning",
+    "nursing scholarship guidance",
+  ],
+};
+let academic_level = [
+  "high school student",
+  "undergraduate student",
+  "graduate student",
+  "other (please specify)",
+];
 export const ConsultSchema = Yup.object().shape({
   fName: Yup.string().required("Required"),
   lName: Yup.string().required("Required"),
@@ -67,13 +99,49 @@ export const ConsultSchema = Yup.object().shape({
     then: (schema) => schema.required("Required"),
   }),
   en_lang_level: Yup.string().when("service", {
-    is: "visa consulting",
+    is: (value) =>
+      value === "visa consulting" || value === "education consulting",
     then: (schema) => schema.required("Required"),
   }),
   visa_country: Yup.string().when("service", {
     is: "visa consulting",
     then: (schema) => schema.required("Required"),
   }),
+  visa_service_type: Yup.string().when("service", {
+    is: "visa consulting",
+    then: (schema) => schema.required("Required"),
+  }),
+
+  // Visa Consulting Ends
+
+  // Edu Consulting Starts
+
+  edu_background: Yup.string().when("service", {
+    is: "education consulting",
+    then: (schema) => schema.required("Required"),
+  }),
+  edu_interest: Yup.string().when("service", {
+    is: "education consulting",
+    then: (schema) => schema.required("Required"),
+  }),
+
+  edu_interest_other: Yup.string().when(["service", "edu_interest"], {
+    is: (service, edu_interest) =>
+      service === "visa consulting" &&
+      edu_interest === "other (please specify)",
+    then: (schema) => schema.required("Please Specify"),
+    // otherwise: (schema) => schema.notRequired(),
+  }),
+
+  cur_acad_level: Yup.string().when("service", {
+    is: "education consulting",
+    then: (schema) => schema.required("Required"),
+  }),
+
+  // Edu consulting Ends
+
+  motivations: Yup.string().required("Required"),
+  goals: Yup.string().required("Required"),
 });
 export default function ContactForm() {
   const { token } = useAuth();
@@ -227,6 +295,37 @@ export default function ContactForm() {
                     </div>
                   </div>
 
+                  {(values?.service == "visa consulting" ||
+                    values?.service == "education consulting") && (
+                    <>
+                      <div className="mt-2 relative">
+                        {/* <label htmlFor="user_id">Choose a User:</label> */}
+                        <InputLabel>Visa Country </InputLabel>
+                        <Field
+                          as="select"
+                          name="visa_country"
+                          className={`${Inputdisplay} capitalize`}
+                        >
+                          <option value="">Select Country</option>
+
+                          {countries?.map((name, i) => {
+                            return (
+                              <option key={i} value={name}>
+                                {name}
+                              </option>
+                            );
+                          })}
+                        </Field>
+
+                        {errors?.visa_country && (
+                          <p className="text-sm text-red-600 mt-2">
+                            {errors?.visa_country}
+                          </p>
+                        )}
+                      </div>
+                    </>
+                  )}
+
                   {/* Fields for Visa Consulting Start*/}
 
                   {values?.service == "visa consulting" && (
@@ -261,52 +360,26 @@ export default function ContactForm() {
 
                       <div className="mt-2 relative">
                         {/* <label htmlFor="user_id">Choose a User:</label> */}
-                        <InputLabel>Visa Country </InputLabel>
-                        <Field
-                          as="select"
-                          name="vise_country"
-                          className={`${Inputdisplay} capitalize`}
-                        >
-                          <option value="">Select Country</option>
-
-                          {countries?.map((name, i) => {
-                            return (
-                              <option key={i} value={name}>
-                                {name}
-                              </option>
-                            );
-                          })}
-                        </Field>
-
-                        {errors?.visa_country && (
-                          <p className="text-sm text-red-600 mt-2">
-                            {errors?.visa_country}
-                          </p>
-                        )}
-                      </div>
-
-                      <div className="mt-2 relative">
-                        {/* <label htmlFor="user_id">Choose a User:</label> */}
                         <InputLabel>Visa Service Type</InputLabel>
-                        <Field
-                          as="select"
-                          name="visa_service_type"
-                          className={`${Inputdisplay} capitalize`}
-                        >
-                          <option value="">Select Service</option>
+                        {values?.visa_country?.length > 1 && (
+                          <Field
+                            as="select"
+                            name="visa_service_type"
+                            className={`${Inputdisplay} capitalize`}
+                          >
+                            <option value="">Select Service</option>
 
-                          {values?.visa_country == "germany"
-                            ? de_visa_type
-                            : values?.visa_country == "uk"
-                            ? uk_visa_type
-                            : india_visa_type?.map((name, i) => {
+                            {visa_types[values?.visa_country]?.map(
+                              (name, i) => {
                                 return (
                                   <option key={i} value={name}>
                                     {name}
                                   </option>
                                 );
-                              })}
-                        </Field>
+                              }
+                            )}
+                          </Field>
+                        )}
 
                         {errors?.visa_service_type && (
                           <p className="text-sm text-red-600 mt-2">
@@ -317,6 +390,94 @@ export default function ContactForm() {
                     </>
                   )}
                   {/* Fields for Visa Consulting Ends */}
+
+                  {/* Fields For Education Consulting Starts*/}
+                  {values?.service == "education consulting" && (
+                    <>
+                      <Input
+                        name="edu_background"
+                        type="textarea"
+                        value={values?.edu_background}
+                        onChange={handleChange}
+                        label="educational background"
+                        errors={errors}
+                      />
+                      <div className="mt-2 relative">
+                        {/* <label htmlFor="user_id">Choose a User:</label> */}
+                        <InputLabel>Area of Interest</InputLabel>
+                        {values?.visa_country?.length > 1 && (
+                          <Field
+                            as="select"
+                            name="edu_interest"
+                            className={`${Inputdisplay} capitalize`}
+                          >
+                            <option value="">Select Service</option>
+
+                            {edu_types[values?.visa_country]?.map((name, i) => {
+                              return (
+                                <option key={i} value={name}>
+                                  {name}
+                                </option>
+                              );
+                            })}
+                          </Field>
+                        )}
+
+                        {errors?.edu_interest && (
+                          <p className="text-sm text-red-600 mt-2">
+                            {errors?.edu_interest}
+                          </p>
+                        )}
+                      </div>
+
+                      {values?.edu_interest == "other (please specify)" && (
+                        <Input
+                          name="edu_interest_other"
+                          value={values?.edu_interest_other}
+                          onChange={handleChange}
+                          label="other (please specify)"
+                          errors={errors}
+                        />
+                      )}
+
+                      <div className="mt-2 relative">
+                        {/* <label htmlFor="user_id">Choose a User:</label> */}
+                        <InputLabel>Current Academic Level</InputLabel>
+
+                        <Field
+                          as="select"
+                          name="cur_acad_level"
+                          className={`${Inputdisplay} capitalize`}
+                        >
+                          <option value="">Select Service</option>
+
+                          {academic_level?.map((name, i) => {
+                            return (
+                              <option key={i} value={name}>
+                                {name}
+                              </option>
+                            );
+                          })}
+                        </Field>
+
+                        {errors?.cur_acad_level && (
+                          <p className="text-sm text-red-600 mt-2">
+                            {errors?.cur_acad_level}
+                          </p>
+                        )}
+                      </div>
+
+                      <Input
+                        name="other_academic_level"
+                        value={values?.other_academic_level}
+                        onChange={handleChange}
+                        label="other academic (please specify)"
+                        errors={errors}
+                      />
+                    </>
+                  )}
+
+                  {/* Education Consulting Ends Here */}
 
                   <div className="">
                     <Input
@@ -329,6 +490,16 @@ export default function ContactForm() {
                           `to come to ${values?.visa_country}`) ||
                         ""
                       }?`}
+                      errors={errors}
+                    />
+                  </div>
+                  <div className="">
+                    <Input
+                      name="goals"
+                      type="textarea"
+                      value={values?.goals}
+                      onChange={handleChange}
+                      label={`What are your future goals upon getting the visa?`}
                       errors={errors}
                     />
                   </div>
