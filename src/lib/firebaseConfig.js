777aae9657deace7e238516641bd27fa3e45b253
tauqeer180 <tauqeermalik855@gmail.com>
2024-07-27 04:25:43 +0500

    fixes

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -13,14 +13,14 @@ const firebaseConfig = {
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_ID,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
   measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+// const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
-export { app, auth, db, storage };
+export { app, db, storage };
--- src/models/Appointment.js
+++ src/models/Appointment.js
@@ -1,181 +1,184 @@
 import mongoose from "mongoose";
-const AppointmentSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: false,
-  },
-  fName: {
-    type: String,
-    required: true,
-  },
-  lName: {
-    type: String,
-  },
-  phone: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  dob: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  service: {
-    type: String,
-    required: true,
-  },
-  high_qualification: {
-    type: String,
-  },
-  de_lang_level: {
-    type: String,
-  },
-  en_lang_level: {
-    type: String,
-  },
-  visa_country: {
-    type: String,
-  },
-  visa_service_type: {
-    type: String,
-  },
+const AppointmentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: false,
+    },
+    fName: {
+      type: String,
+      required: true,
+    },
+    lName: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    dob: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    service: {
+      type: String,
+      required: true,
+    },
+    high_qualification: {
+      type: String,
+    },
+    de_lang_level: {
+      type: String,
+    },
+    en_lang_level: {
+      type: String,
+    },
+    visa_country: {
+      type: String,
+    },
+    visa_service_type: {
+      type: String,
+    },
 
-  // Visa Consulting Ends
+    // Visa Consulting Ends
 
-  // Edu Consulting Starts
+    // Edu Consulting Starts
 
-  edu_background: {
-    type: String,
-  },
-  edu_interest: {
-    type: String,
-  },
-  edu_interest_other: {
-    type: String,
-  },
-  cur_acad_level: {
-    type: String,
-  },
-  other_acad_level: {
-    type: String,
-  },
-  quest_or_conc: {
-    // Question or Concern
-    type: String,
-  },
-  additional_info: {
-    type: String,
-  },
-  // Edu Consulting Ends
-  //   Career Consulting Start
+    edu_background: {
+      type: String,
+    },
+    edu_interest: {
+      type: String,
+    },
+    edu_interest_other: {
+      type: String,
+    },
+    cur_acad_level: {
+      type: String,
+    },
+    other_acad_level: {
+      type: String,
+    },
+    quest_or_conc: {
+      // Question or Concern
+      type: String,
+    },
+    additional_info: {
+      type: String,
+    },
+    // Edu Consulting Ends
+    //   Career Consulting Start
 
-  field_of_study: {
-    type: String,
-  },
-  career_interest: {
-    type: String,
-  },
-  other_career_interest: {
-    type: String,
-  },
-  preferred_countries: {
-    type: String,
-  },
-  relev_expe: {
-    type: String,
-  },
+    field_of_study: {
+      type: String,
+    },
+    career_interest: {
+      type: String,
+    },
+    other_career_interest: {
+      type: String,
+    },
+    preferred_countries: {
+      type: String,
+    },
+    relev_expe: {
+      type: String,
+    },
 
-  // Career consulting Ends
+    // Career consulting Ends
 
-  // Medical consulting Starts
+    // Medical consulting Starts
 
-  age: {
-    type: String,
-  },
-  med_history: {
-    type: String,
-  },
-  med_area_concern: {
-    type: String,
-  },
-  med_symp_issues: {
-    type: String,
-  },
-  prev_treat_med: {
-    type: String,
-  },
-  curr_treat_med: {
-    type: String,
-  },
-  allergies: {
-    type: String,
-  },
-  lifestyle_habbits: {
-    type: String,
-  },
-  // Medical COnsulting Ends,
-  // IT Consulting Starts
+    age: {
+      type: String,
+    },
+    med_history: {
+      type: String,
+    },
+    med_area_concern: {
+      type: String,
+    },
+    med_symp_issues: {
+      type: String,
+    },
+    prev_treat_med: {
+      type: String,
+    },
+    curr_treat_med: {
+      type: String,
+    },
+    allergies: {
+      type: String,
+    },
+    lifestyle_habbits: {
+      type: String,
+    },
+    // Medical COnsulting Ends,
+    // IT Consulting Starts
 
-  it_service_type: {
-    type: String,
-  },
-  other_it_service_type: {
-    type: String,
-  },
-  proj_desc: {
-    type: String,
-  },
-  proj_status: {
-    type: String,
-  },
-  expect_start_date: {
-    type: String,
-  },
-  add_comm_q: {
-    // Additional Comments and Questions
-    type: String,
-  },
+    it_service_type: {
+      type: String,
+    },
+    other_it_service_type: {
+      type: String,
+    },
+    proj_desc: {
+      type: String,
+    },
+    proj_status: {
+      type: String,
+    },
+    expect_start_date: {
+      type: String,
+    },
+    add_comm_q: {
+      // Additional Comments and Questions
+      type: String,
+    },
 
-  // IT Consulting Ends
-  motivations: {
-    type: String,
-  },
-  goals: {
-    type: String,
-  },
-  file_cnic: {
-    type: String,
-  },
-  file_passport: {
-    type: String,
-  },
-  file_degrees: {
-    type: String,
-  },
-  file_exp_letter: {
-    // Experience Letter
-    type: String,
-  },
-  file_lang_cert: {
-    type: String,
-  },
-  file_addit_docs: {
-    type: String,
-  },
+    // IT Consulting Ends
+    motivations: {
+      type: String,
+    },
+    goals: {
+      type: String,
+    },
+    file_cnic: {
+      type: String,
+    },
+    file_passport: {
+      type: String,
+    },
+    file_degrees: {
+      type: String,
+    },
+    file_exp_letter: {
+      // Experience Letter
+      type: String,
+    },
+    file_lang_cert: {
+      type: String,
+    },
+    file_addit_docs: {
+      type: String,
+    },
 
-  status: {
-    type: Boolean,
-    default: false,
+    status: {
+      type: Boolean,
+      default: false,
+    },
   },
-});
+  { timestamps: true }
+);
 
 export default mongoose.models.Appointments ||
   mongoose.model("Appointments", AppointmentSchema);
